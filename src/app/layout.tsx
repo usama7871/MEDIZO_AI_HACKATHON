7a00a18dd57d07f91f25888785111406ea5b0f2e
USAMA ABDULLAH <kusamakhan1234@gmail.com>
2025-10-17 15:04:49 +0500

    Import Analytics component in layout.tsx
    Added Analytics component from Vercel to layout.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type {Metadata} from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 import { AppProviders } from '@/components/app-providers';
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: 'Medizo AI - Patient Simulator',
